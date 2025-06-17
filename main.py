7b77cd726a74371d3efec1549328ef9da0dea9aa
jortorres <jordan.torres1@gmail.com>
2025-06-16 23:47:25 -0500

    Added the "--vebose" flag

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY") # get API
-client = genai.Client(api_key=api_key) # create an instance
+def get_prompt_from_arg():  # funciton to get request from console
+    arg_num = len(sys.argv)
+    if arg_num < 2:
+        print("Usage: python3 main.py '<prompt>'")
+        sys.exit(1)
+    elif arg_num < 3:
+        return sys.argv[1]
+    if arg_num == 3 and sys.argv[2] == "--verbose":
+        return sys.argv[1] 
+    else:
+        sys.exit(1)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
-print(response.text)
+def main():
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY") # get API
+    client = genai.Client(api_key=api_key) # create an instance
+
+    arg_prompt = get_prompt_from_arg()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=arg_prompt)]),] # put messages from prompt into a list
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)  # added message and send to gemini
+
+    if len(sys.argv)== 3 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {arg_prompt}") # print response
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}") # print prmpt tokens
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n") # print response tokens
+        print(response.text)
+    else:
+        print(response.text) # print response        
 
 
+if __name__ == "__main__":
+     main()
+     
